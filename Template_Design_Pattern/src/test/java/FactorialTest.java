9d082dbf4c55866ffafa3bb5b4340ab84ad7b7fd
Nikita Nikolayuk <113298204+minimaledit@users.noreply.github.com>
2023-01-13 16:54:14 +0300

    Add files via upload

---
+++ Template_Design_Pattern/src/test/java/FactorialTest.java
@@ -0,0 +1,32 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FactorialTest {
+    @Test
+    public void testPositiveInput() {
+        Factorial factorial = new FactorialImpl();
+        long result = factorial.calculate(5);
+        Assert.assertEquals(120, result);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNegativeInput() {
+        Factorial factorial = new FactorialImpl();
+        factorial.calculate(-5);
+    }
+
+    @Test
+    public void testZeroInput() {
+        Factorial factorial = new FactorialImpl();
+        long result = factorial.calculate(0);
+        Assert.assertEquals(1, result);
+    }
+
+    @Test
+    public void testHugeInput() {
+        Factorial factorial = new FactorialImpl();
+        long result = factorial.calculate(20);
+        long expected = 2432902008176640000;
+        Assert.assertEquals(expected, result);
+    }
+}
